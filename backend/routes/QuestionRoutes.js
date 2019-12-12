71940aa30f3ca467e8fec91db7ea7f2a62d657a3
saruit0105 <saruits2@gmail.com>
2019-12-12 10:47:26 -0500

    backend route

--- backend/routes/QuestionRoutes.js
+++ backend/routes/QuestionRoutes.js
@@ -5,6 +5,7 @@ const Question = require("../models/Question");
 router.get("/getQuestions", async (req, res, next) => {
   try {
     const questions = await Question.find({ catagory });
+    res.json(questions);
   } catch (err) {
     next(err);
   }
