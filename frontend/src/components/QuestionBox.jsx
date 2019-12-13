12820e017d539d1b863fe764e2910be161e95255
saruit0105 <saruits2@gmail.com>
2019-12-13 14:45:22 -0500

    cleaned up useless code and questions page working a lot cleaner

--- frontend/src/components/QuestionBox.jsx
+++ frontend/src/components/QuestionBox.jsx
@@ -1,41 +1,6 @@
 import React, { Component } from "react";
 
 class QuestionBox extends Component {
-  state = {
-    picked: false,
-    isCorrect: false
-  };
-
-  pickHandler = (e, eachAnswers, solution) => {
-    e.preventDefault();
-    console.log(solution);
-    console.log(eachAnswers);
-    let questionCheck = false;
-    eachAnswers === solution ? (questionCheck = true) : (questionCheck = false);
-    this.setState({
-      picked: !this.state.picked,
-      isCorrect: questionCheck
-    });
-    console.log(this.state);
-  };
-  choicePicked = () => {
-    if (this.state.picked === true && this.state.isCorrect === true) {
-      return (
-        <div>
-          <p>wow you are right! </p>
-          <button> Next Question</button>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <p> Choose the correct answer below</p>
-          <button> Next Question</button>
-        </div>
-      );
-    }
-  };
-
   render() {
     return (
       <div>
@@ -45,14 +10,14 @@ class QuestionBox extends Component {
               ? this.props.randomQuestion.question
               : ""}
           </strong>
-          {this.choicePicked()}
+          {this.props.choicePicked()}
           {this.props.randomQuestion &&
             this.props.randomQuestion.answers.map(eachAnswers => (
               <div className="questionBox">
                 <div key={this.props.randomQuestion.answers}>
                   <span
                     onClick={e =>
-                      this.pickHandler(
+                      this.props.pickHandler(
                         e,
                         eachAnswers,
                         this.props.randomQuestion.solution
--- frontend/src/components/Questions.jsx
+++ frontend/src/components/Questions.jsx
@@ -6,8 +6,10 @@ import { QuestionBox } from ".";
 class Questions extends Component {
   state = {
     questions: null,
-
-    randomQuestion: null
+    picked: false,
+    isCorrect: false,
+    randomQuestion: null,
+    counter: 1
   };
 
   fetchQuestions = async () => {
@@ -24,13 +26,21 @@ class Questions extends Component {
   componentDidMount = async () => {
     await this.fetchQuestions();
     await this.pickQuestion();
+    await this.countCheck();
   };
 
   nextQuestion = () => {
+    let nextCount = this.state.counter + 1;
     console.log("Next one");
-    this.setState({
-      next: true
-    });
+    this.pickQuestion();
+    this.countCheck();
+    this.setState({ picked: false, isCorrect: false, counter: nextCount });
+  };
+
+  countCheck = () => {
+    this.state.counter > 4
+      ? alert("Good job on finishing the quiz")
+      : console.log("youre not done yet");
   };
 
   pickQuestion = () => {
@@ -40,11 +50,59 @@ class Questions extends Component {
     this.setState({ randomQuestion: randomQ });
   };
 
+  pickHandler = (e, eachAnswers, solution) => {
+    e.preventDefault();
+    console.log(solution);
+    console.log(eachAnswers);
+    let questionCheck = false;
+    eachAnswers === solution ? (questionCheck = true) : (questionCheck = false);
+    this.setState({
+      picked: !this.state.picked,
+
+      isCorrect: questionCheck
+    });
+    console.log(this.state);
+  };
+
+  choicePicked = () => {
+    if (this.state.picked === true && this.state.isCorrect === true) {
+      return (
+        <div>
+          <p>wow you are right! </p>
+        </div>
+      );
+    } else if (this.state.picked === true && this.state.isCorrect === false) {
+      return (
+        <div>
+          <p>
+            {" "}
+            Sorry, that's the wrong answer, the correct answer is :
+            {this.state.randomQuestion.solution}
+          </p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <p> Choose the correct answer below</p>
+        </div>
+      );
+    }
+  };
+
   render() {
     return (
       <div>
-        <p> Question 1 out of 5</p>
-        <QuestionBox randomQuestion={this.state.randomQuestion} />
+        <p> Question {this.state.counter} out of 5</p>
+        <button onClick={() => this.nextQuestion()}> Go Next </button>
+        <QuestionBox
+          randomQuestion={this.state.randomQuestion}
+          pickQuestion={this.state.pickQuestion}
+          picked={this.state.picked}
+          isCorrect={this.state.isCorrect}
+          pickHandler={this.pickHandler}
+          choicePicked={this.choicePicked}
+        />
       </div>
     );
   }
