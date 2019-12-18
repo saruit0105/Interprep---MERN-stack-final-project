2a1e3adfa465c44e62c4b9b4c7b644bca4681c7a
saruit0105 <saruits2@gmail.com>
2019-12-18 15:50:39 -0500

    landing is now a class

--- frontend/src/pages/landing/Landing.page.jsx
+++ frontend/src/pages/landing/Landing.page.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./Landing.page.css";
 import FISH_LOGO from "../../images/fish.png";
@@ -64,65 +64,68 @@ const links = [
     difficulty: ["Easy", "Medium", "Hard"]
   }
 ];
-const Landing = () => <UserContext.Consumer>{context => <Component context={context} />}</UserContext.Consumer>;
 
-const Component = ({ context }) => {
-  const { currentUser } = context;
-  return (
-    <div>
-      <header className="masthead bg-primary text-white text-center">
-        <div className="container d-flex align-items-center flex-column">
-          <img className="masthead-avatar mb-5" src={FISH_LOGO} alt="" />
-          <h1 className="masthead-heading text-uppercase mb-0">Welcome {currentUser.name}</h1>
-          <div className="divider-custom divider-light">
-            <div className="divider-custom-line"></div>
-            <div className="divider-custom-icon">
-              <i className="fas fa-star"></i>
-            </div>
-            <div className="divider-custom-line"></div>
-          </div>
-          <p className="masthead-subheading font-weight-light mb-0">Graphic Artist - Web Designer - Illustrator</p>
-        </div>
-      </header>
+class Landing extends Component {
+  static contextType = UserContext;
 
-      <section className="page-section portfolio" id="portfolio">
-        <div className="container">
-          <h2 className="page-section-heading text-center text-uppercase text-secondary mb-0">Quiz Types</h2>
-
-          <div className="divider-custom">
-            <div className="divider-custom-line"></div>
-            <div className="divider-custom-icon">
-              <i className="fas fa-star"></i>
-            </div>
-            <div className="divider-custom-line"></div>
-          </div>
-
-          <div className="row">
-            {links.map(({ label, path, category, picture }) => (
-              <div className="col-md-6 col-lg-4" key={label}>
-                <div className="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal1">
-                  <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                    <div className="portfolio-item-caption-content text-center text-white">
-                      <i className="fas fa-plus fa-3x"></i>
-                    </div>
-                  </div>
-                  <button className="picButton">
-                    <h3 className="labels">{label}</h3>
-                    <img className="img-fluid" src={picture} alt="" />
-                    {category.map(({ links, difficulty }) => (
-                      <Link to={`/quiz/${links}`} key={links}>
-                        {difficulty}
-                      </Link>
-                    ))}
-                  </button>
-                </div>
+  render() {
+    const { currentUser } = this.context;
+    return (
+      <div>
+        <header className="masthead bg-primary text-white text-center">
+          <div className="container d-flex align-items-center flex-column">
+            <img className="masthead-avatar mb-5" src={FISH_LOGO} alt="" />
+            <h1 className="masthead-heading text-uppercase mb-0">Welcome {currentUser.name}</h1>
+            <div className="divider-custom divider-light">
+              <div className="divider-custom-line"></div>
+              <div className="divider-custom-icon">
+                <i className="fas fa-star"></i>
               </div>
-            ))}
+              <div className="divider-custom-line"></div>
+            </div>
+            <p className="masthead-subheading font-weight-light mb-0">Graphic Artist - Web Designer - Illustrator</p>
           </div>
-        </div>
-      </section>
-    </div>
-  );
-};
+        </header>
+
+        <section className="page-section portfolio" id="portfolio">
+          <div className="container">
+            <h2 className="page-section-heading text-center text-uppercase text-secondary mb-0">Quiz Types</h2>
+
+            <div className="divider-custom">
+              <div className="divider-custom-line"></div>
+              <div className="divider-custom-icon">
+                <i className="fas fa-star"></i>
+              </div>
+              <div className="divider-custom-line"></div>
+            </div>
+
+            <div className="row">
+              {links.map(({ label, path, category, picture }) => (
+                <div className="col-md-6 col-lg-4" key={label}>
+                  <div className="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal1">
+                    <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                      <div className="portfolio-item-caption-content text-center text-white">
+                        <i className="fas fa-plus fa-3x"></i>
+                      </div>
+                    </div>
+                    <button className="picButton">
+                      <h3 className="labels">{label}</h3>
+                      <img className="img-fluid" src={picture} alt="" />
+                      {category.map(({ links, difficulty }) => (
+                        <Link to={`/quiz/${links}`} key={links}>
+                          {difficulty}
+                        </Link>
+                      ))}
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
 
 export default Landing;
