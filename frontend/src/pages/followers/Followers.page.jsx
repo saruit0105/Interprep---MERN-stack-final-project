354fc9ad6ed8f099b55278d42e76586f25cfb35f
saruit0105 <saruits2@gmail.com>
2019-12-19 15:52:31 -0500

    follower page worked on

--- frontend/src/pages/followers/Followers.page.jsx
+++ frontend/src/pages/followers/Followers.page.jsx
@@ -1,21 +1,43 @@
 import React from "react";
 import { UserContext } from "../../context/UserContext";
 import "./Followers.page.css";
+import classNames from "classnames";
+import Table from "react-bootstrap/Table";
 
 const Followers = () => <UserContext.Consumer>{context => <Component context={context} />}</UserContext.Consumer>;
 
 const Component = ({ context }) => {
   const { currentUser } = context;
   return (
-    <div>
-      <p>Followers</p>
-      {currentUser.followers &&
-        currentUser.followers.map(({ name, points }) => (
-          <div>
-            <p>{name}</p>
-            <p>Points: {points}</p>
-          </div>
-        ))}
+    <div className="body">
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Points</th>
+            <th>Badges</th>
+            <th>Github</th>
+            <th>Linkedin</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentUser.followers.map(({ name, points, badges, github, linkedin }, index) => {
+            return (
+              <tr>
+                <td>{index + 1}</td>
+                <td>
+                  <strong>{name}</strong>
+                </td>
+                <td>{points || 0}</td>
+                <td>{badges && badges.map(eachBadge => <img src={eachBadge} className="badges" />)}</td>
+                <td>{github}</td>
+                <td>{linkedin}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
     </div>
   );
 };
